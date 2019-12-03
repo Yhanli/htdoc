a73b26877a5ddb2d90fdf6472355d36646097739
yhanli <leeyuhan@qq.com>
2019-12-03 15:08:12 +1300

    44

--- sfcourse/src/Controller/MainController.php
+++ sfcourse/src/Controller/MainController.php
@@ -3,18 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('main/index.html.twig', [
-            'message' => 'new symfony',
-            'controller_name' => 'MainController',
-        ]);
+        return new Response('<h1> Hello there</h1>');
+    }
+
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     */
+    public function custom(Request $request){
+        dump($request);
+        return new Response('<h1>Welcome !</h1>');
     }
 }
